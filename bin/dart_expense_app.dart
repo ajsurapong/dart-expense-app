bcbec921c12d5afeaec8fb7f68eab6ac53c3e9fe
ajsurapong <surapong@mfu.ac.th>
2025-08-22 11:06:38 +0700

    initial commit

---
+++ bin/dart_expense_app.dart
@@ -0,0 +1,5 @@
+import 'package:dart_expense_app/dart_expense_app.dart' as dart_expense_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_expense_app.calculate()}!');
+}
---
+++ test/dart_expense_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_expense_app/dart_expense_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
